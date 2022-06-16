faf6b2551f4ba24b2546cf29723834bee59765e5
Patrick Stephens <pat@calyptia.com>
2022-06-16 11:37:05 +0100

    More metrics

--- compose/run.sh
+++ compose/run.sh
@@ -11,9 +11,10 @@ QUERY_RANGE=${QUERY_RANGE:-5m}
 END=$((SECONDS+300))
 
 declare -a QUERY_METRICS=("fluentbit_input_records_total"
-                          "fluentbit_output_records_total"
+                          "fluentbit_output_proc_records_total"
+                          "fluentbit_output_dropped_records_total"
+                          "fluentbit_output_errors_total"
 )
-./stop.sh
 
 if [[ ! -x "helpers/promplot" ]]; then
     curl --fail --silent -L https://github.com/qvl/promplot/releases/download/v0.17.0/promplot_0.17.0_linux_64bit.tar.gz| tar -xz
@@ -22,13 +23,19 @@ if [[ ! -x "helpers/promplot" ]]; then
     mv -fv ./promplot helpers/promplot
 fi
 
+# cleanup
+./stop.sh
+rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
 
+# start
 $DOCKER_COMPOSE_CMD up --force-recreate -d
 
 while [ $SECONDS -lt $END ]; do
     if [ -z "$($DOCKER_COMPOSE_CMD ps -q "$SERVICE_TO_MONITOR")" ] || [ -z "$(docker ps -q --no-trunc | grep "$($DOCKER_COMPOSE_CMD ps -q "$SERVICE_TO_MONITOR")")" ]; then
         echo "Container has failed after $SECONDS seconds"
         $DOCKER_COMPOSE_CMD logs &> "$OUTPUT_DIR/run.log"
+        $DOCKER_COMPOSE_CMD logs "$SERVICE_TO_MONITOR" &> "$OUTPUT_DIR/failed.log"
         exit 1
     fi
     sleep 10
@@ -38,7 +45,6 @@ done
 $DOCKER_COMPOSE_CMD logs &> "$OUTPUT_DIR/run.log"
 curl -XPOST "$PROM_URL/api/v1/admin/tsdb/snapshot"
 
-mkdir -p "$OUTPUT_DIR"
 for METRIC in "${QUERY_METRICS[@]}"; do
     helpers/promplot -query "$METRIC" -range "$QUERY_RANGE" -url "$PROM_URL" -file "$OUTPUT_DIR/$METRIC.png"
 done
