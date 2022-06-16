793859d3954ce5b501428d4ef19ee2caa13ef37b
Patrick Stephens <pat@calyptia.com>
2022-06-16 10:59:15 +0100

    Tweak to five minutes

--- compose/run.sh
+++ compose/run.sh
@@ -4,8 +4,11 @@ set -eu
 SERVICE_TO_MONITOR=${SERVICE_TO_MONITOR:-fb-delta}
 OUTPUT_DIR=${OUTPUT_DIR:-output}
 PROM_URL=${PROM_URL:-http://localhost:9090}
-QUERY_RANGE=${QUERY_RANGE:-5m}
 DOCKER_COMPOSE_CMD=${DOCKER_COMPOSE_CMD:-docker-compose -f fluent-delta-stack.yml -f monitoring-stack.yml}
+
+# Run for 5 minutes
+QUERY_RANGE=${QUERY_RANGE:-5m}
+END=$((SECONDS+300))
 
 declare -a QUERY_METRICS=("fluentbit_input_records_total")
 
@@ -18,19 +21,18 @@ if [[ ! -x "helpers/promplot" ]]; then
     mv -fv ./promplot helpers/promplot
 fi
 
-end=$((SECONDS+60))
 
 $DOCKER_COMPOSE_CMD up --force-recreate -d
 
-while [ $SECONDS -lt $end ]; do
+while [ $SECONDS -lt $END ]; do
     if [ -z "$($DOCKER_COMPOSE_CMD ps -q "$SERVICE_TO_MONITOR")" ] || [ -z "$(docker ps -q --no-trunc | grep "$($DOCKER_COMPOSE_CMD ps -q "$SERVICE_TO_MONITOR")")" ]; then
         echo "Container has failed after $SECONDS seconds"
-        $DOCKER_COMPOSE_CMD logs
+        $DOCKER_COMPOSE_CMD logs &> "$OUTPUT_DIR/run.log"
         exit 1
     fi
     sleep 10
 done
-$DOCKER_COMPOSE_CMD logs
+$DOCKER_COMPOSE_CMD logs &> "$OUTPUT_DIR/run.log"
 
 mkdir -p "$OUTPUT_DIR"
 curl -XPOST "$PROM_URL/api/v1/admin/tsdb/snapshot"
@@ -38,4 +40,6 @@ for METRIC in "${QUERY_METRICS[@]}"; do
     helpers/promplot -query "$METRIC" -range "$QUERY_RANGE" -url "$PROM_URL" -file "$OUTPUT_DIR/$METRIC.png"
 done
 
-./stop.sh
+if [[ -n "${SKIP_TEARDOWN:-}" ]]; then
+    ./stop.sh
+fi
