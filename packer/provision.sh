9261ef9bd30ff226556345b5f454798de9b9d680
Patrick Stephens <pat@calyptia.com>
2022-06-17 17:09:51 +0100

    Packer updates

--- packer/provision.sh
+++ packer/provision.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 sudo sh <<SCRIPT
+
+# Set up basic installation
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get upgrade -y
 
+# Set up Docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update
 apt-cache policy docker-ce
 
-apt-get install -y docker-ce
+# Add any tools we need
+apt-get install -y docker-ce git
 systemctl status docker
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Install docker-compose
+# Later versions have an SSL issue on this OS
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 
+# Add extra user
 adduser perf-test
 usermod -aG sudo perf-test
 usermod -aG docker perf-test
